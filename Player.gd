7860c9b0131e033f02e9220fca6bd6b59e88d4f2
Kurt Taylor <kurt.a.taylor@gmail.com>
2023-09-26 22:27:13 -0400

    Workin

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const ACCELERATION = 200.0
+const SPEED = 80.0
+const ACCELERATION = 500.0
 
 @onready var reticleHolder = $ReticleHolder
 @onready var reticle = $ReticleHolder/Reticle
@@ -28,16 +28,15 @@ func select_tool(tool):
 		ToolManager.Tool.SOUTH:
 			toolNameLabel.text = "none"
 
-func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+func _process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, delta * ACCELERATION)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, delta * ACCELERATION)
 
 	if direction.length_squared() > 0:
 		reticleHolder.rotation = direction.angle()
 		reticle.rotation = -direction.angle()
 	
-
 	move_and_slide()
